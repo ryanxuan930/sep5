3dceca909b1cc4b8e968824d3f2e65aa4c4b6099
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-01-21 22:03:09 +0800

    Update auth.php

--- server/config/auth.php
+++ server/config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'api',
+        'guard' => 'web',
         'passwords' => 'users',
     ],
 
@@ -73,6 +73,10 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
         ],
 
         // 'users' => [
