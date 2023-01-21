902b7b4ae7764c1200d82290b6e777e6eead79a1
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-01-21 21:57:37 +0800

    Update api.php

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    // admin
+    /*
+    Route::group([
+        'prefix' => '/admin'
+    ], function () {
+        Route::post('/login', [AdminController::class, 'login']);
+        Route::get('/info', [AdminController::class, 'info']);
+        Route::post('/logout', [AdminController::class, 'logout']);
+    });
+    */
+    //user
+    Route::group([
+        'prefix' => '/user'
+    ], function () {
+        Route::post('/login', [UserController::class, 'login']);
+        /*
+        Route::get('/info', [UserController::class, 'info']);
+        Route::patch('/edit/{u_id}', [UserController::class, 'edit']); // invalid
+        Route::post('/logout', [UserController::class, 'logout']);
+        Route::post('/register', [UserController::class, 'register']);
+        Route::get('/exist/{account}', [UserController::class, 'exist']);
+        Route::group([
+            'prefix' => '/verify'
+        ], function () {
+            Route::post('/email', [UserController::class, 'sendVerifyMail']);
+            Route::post('/email/code', [UserController::class, 'codeVerify']);
+        });
+        Route::post('/reset', [UserController::class, 'reset']);
+        Route::post('/reset/password/{account}/{token}', [UserController::class, 'resetPassword']);
+        Route::post('/upload', [UserController::class, 'upload']);
+        */
+    });
 });
