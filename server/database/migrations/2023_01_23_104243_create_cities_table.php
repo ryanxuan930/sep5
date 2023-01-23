266c76036f828a077da21be05a229342f7775956
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-01-23 18:50:51 +0800

    add city table

---
+++ server/database/migrations/2023_01_23_104243_create_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id('city_id');
+            $table->char('city_code', 2);
+            $table->char('area', 1);
+            $table->string('city_name_ch', 4);
+            $table->string('city_name_en', 32);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+};
