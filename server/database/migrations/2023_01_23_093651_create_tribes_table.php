c7b392612cb2f52e3b19942ac8f5dda0a98c7af9
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-01-23 17:42:55 +0800

    add tribe table

---
+++ server/database/migrations/2023_01_23_093651_create_tribes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tribes', function (Blueprint $table) {
+            $table->id('tribe_id');
+            $table->string('tribe_name_ch', 32);
+            $table->string('tribe_name', 128);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tribes');
+    }
+};
