91368406c1b5df00207a069e60bedb5845145df2
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-01-23 23:41:14 +0800

    Update DepartmentController.php

--- server/app/Http/Controllers/DepartmentController.php
+++ server/app/Http/Controllers/DepartmentController.php
@@ -22,7 +22,7 @@ class DepartmentController extends Controller
      */
     public function index()
     {
-        $user = auth()->user();
+        $user = auth('admin')->user();
         return response()->json(['status'=>$user]);
         //return response()->json(Department::leftJoin('organizations', 'departments.related_org_id', '=', 'organizations.org_id')->orderBy('related_org_id', 'asc')->orderBy('sort_order', 'asc')->get());
     }
