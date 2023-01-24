0e186cbb6e2c62af832f9f5b9cc887f99f91972e
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-01-25 00:54:31 +0800

    rename game tag model

--- server/app/Models/GameTag.php
+++ server/app/Models/GameTag.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class GameTags extends Model
+class GameTag extends Model
 {
     use HasFactory;
     protected $primaryKey = "game_tag_id";
