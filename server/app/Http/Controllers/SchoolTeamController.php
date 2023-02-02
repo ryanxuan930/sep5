b08e4431cfb1be3e6c2bb74f6aa53988ed8dff27
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-02 13:56:41 +0800

    Update SchoolTeamController.php

--- server/app/Http/Controllers/SchoolTeamController.php
+++ server/app/Http/Controllers/SchoolTeamController.php
@@ -21,7 +21,7 @@ class SchoolTeamController extends Controller
      */
     public function index($org_id)
     {
-        $query = SchoolTeam::leftJoin('organizations', 'organizations.org_id', '=', 'school_teams.org_id')->select('school_teams.*', 'organizations.org_id', 'organizations.org_name_full_ch', 'organizations.org_name_ch', 'organizations.org_name_full_en', 'organizations.org_name_en');
+        $query = SchoolTeam::leftJoin('organizations', 'organizations.org_id', '=', 'school_teams.org_id')->leftJoin('sport_lists', 'sport_lists.sport_id', '=', 'school_teams.sport_id')->select('school_teams.*', 'organizations.org_id', 'organizations.org_name_full_ch', 'organizations.org_name_ch', 'organizations.org_name_full_en', 'organizations.org_name_en', 'sport_lists.sport_id', 'sport_lists.sport_name_ch', 'sport_lists.sport_name_en', 'sport_lists.sport_code');
         if ($org_id == 1) {
             return response()->json($query->get());
         } else {
@@ -63,7 +63,7 @@ class SchoolTeamController extends Controller
      */
     public function show($org_id, $id)
     {
-        $query = SchoolTeam::leftJoin('organizations', 'organizations.org_id', '=', 'school_teams.org_id')->select('school_teams.*', 'organizations.org_id', 'organizations.org_name_full_ch', 'organizations.org_name_ch', 'organizations.org_name_full_en', 'organizations.org_name_en');
+        $query = SchoolTeam::leftJoin('organizations', 'organizations.org_id', '=', 'school_teams.org_id')->leftJoin('sport_lists', 'sport_lists.sport_id', '=', 'school_teams.sport_id')->select('school_teams.*', 'organizations.org_id', 'organizations.org_name_full_ch', 'organizations.org_name_ch', 'organizations.org_name_full_en', 'organizations.org_name_en', 'sport_lists.sport_id', 'sport_lists.sport_name_ch', 'sport_lists.sport_name_en', 'sport_lists.sport_code');
         return response()->json($query->where('school_team_id', $id)->get());
     }
 
