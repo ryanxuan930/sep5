21ca4d398e99e7c2de906469afd0822c4d1e2a85
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-03 14:15:20 +0800

    Create ConfigController.php

---
+++ server/app/Http/Controllers/ConfigController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Config;
+
+class ConfigController extends Controller
+{
+    // construct
+    public function __construct()
+    {
+        $this->middleware('auth:admin', ['except' => ['index', 'show']]);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Config::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'reg_switch' => 'required|boolean',
+            'reg_content' => 'nullable',
+            'root_page' => 'required|boolean',
+            'multilingual' => 'required|boolean',
+            'options' => 'nullable',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $temp = $request->all();
+        $temp['created_at'] = date("Y-m-d H:i:s");
+        $temp['updated_at'] = date("Y-m-d H:i:s");
+        Config::insert($temp);
+        return response()->json(['status'=>'A01']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json(Config::where('config_id', $id)->first());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(),[
+            'reg_switch' => 'required|boolean',
+            'reg_content' => 'nullable',
+            'root_page' => 'required|boolean',
+            'multilingual' => 'required|boolean',
+            'options' => 'nullable',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $temp = $request->all();
+        $temp['updated_at'] = date("Y-m-d H:i:s");
+        Config::where('config_id', $id)->update($temp);
+        return response()->json(['status'=>'A01']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Event::where('config_id', $id)->delete();
+        return response()->json(['status'=>'A01']);
+    }
+}
