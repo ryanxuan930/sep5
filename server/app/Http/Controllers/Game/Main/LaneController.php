43fbe178faa6bc9979c2eb76cd0f58bdf10f12aa
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-03 12:32:59 +0800

    Update LaneController.php

--- server/app/Http/Controllers/Game/Main/LaneController.php
+++ server/app/Http/Controllers/Game/Main/LaneController.php
@@ -4,8 +4,49 @@ namespace App\Http\Controllers\Game\Main;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
+use Illuminate\Support\Facades\DB;
 
 class LaneController extends Controller
 {
-    //
+    private $tableName = 'divisions';
+    // construct
+    public function __construct()
+    {
+        $this->middleware('auth:admin', ['except' => ['getters']]);
+    }
+    /**
+     * Display a listing of the resource.
+     * 
+     * @param  string $sportCode
+     * @param  string $orgId
+     * @return \Illuminate\Http\Response
+     */
+    public function getters($sportCode, $gameId)
+    {
+        return response()->json(DB::table($sportCode.'_'.$gameId.'_'.$this->tableName)->get());
+    }
+    /**
+     * Set a series of resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string $sportCode
+     * @param  string $orgId
+     * @return \Illuminate\Http\Response
+     */
+    public function setters(Request $request, $sportCode, $gameId)
+    {
+        // validation
+        $validator = Validator::make($request->all(),[
+            '*.straight' => 'required|integer',
+            '*.round' => 'required|integer',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $temp = $request->all();
+        DB::table($sportCode.'_'.$gameId.'_'.$this->tableName)->truncate();
+        DB::table($sportCode.'_'.$gameId.'_'.$this->tableName)->insert($temp);
+        return response()->json(['status'=>'A01']);
+    }
 }
