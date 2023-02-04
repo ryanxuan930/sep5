1d9830e0472ed5cf4732c98224f8a1ba0bf0bfdd
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-04 11:46:08 +0800

    Update api.php

--- server/routes/api.php
+++ server/routes/api.php
@@ -83,7 +83,7 @@ Route::group([
 
     Route::apiResource('/game', GameController::class);
     Route::get('/game-all', [GameController::class, 'indexAll']);
-    Route::get('/game-by-tag', [GameController::class, 'indexByTag']);
+    Route::get('/game-by-tag/{gameTag}', [GameController::class, 'indexByTag']);
 
     Route::apiResource('/game-tag', GameTagController::class);
 
