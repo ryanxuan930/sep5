736e8c1136be98ea807d95e275e9149618655eea
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-08 19:22:51 +0800

    Update UserController.php

--- server/app/Http/Controllers/Auth/UserController.php
+++ server/app/Http/Controllers/Auth/UserController.php
@@ -47,39 +47,43 @@ class UserController extends Controller
 
         // find user in User table
         $findUser = User::where('account', $request->all()['account'])->first();
-        if (env('USE_MONKEYID')) { // if using monkey id
-            $response = HttpsRequest::post('https://sports.nsysu.edu.tw/monkeyserver/api/app/login/'.env('MONKEYID_KEY'), $request->all());
-            if ($response['status'] == 'A02') { // check response
-                if (is_null($findUser)) { // add to user table
-                    $temp = [
-                        'account' => $response['account'],
-                        'monkey_user_id' => $response['monkey_user_id'],
-                        'user_identity' => $response['user_identity'],
-                        'first_name_ch' => $response['name'],
-                        'org_code' => $response['org_code'],
-                        'password' => password_hash($request->all()['password'], PASSWORD_DEFAULT),
-                        'is_student' => $response['user_identity'] == 0 ? 1 : 0,
-                        'created_at' => $loginTime,
-                        'updated_at' => $loginTime,
-                        'athlete_id' => strtoupper(str_pad(base_convert(floor(microtime(true)*100), 10, 36), 8, '0', STR_PAD_LEFT)),
-                    ];
-                    User::insert($temp);
-                } else { // update user table
-                    $temp = [
-                        'account' => $response['account'],
-                        'user_identity' => $response['user_identity'],
-                        'org_code' => $response['org_code'],
-                        'is_student' => $response['user_identity'] == 0 ? 1 : 0,
-                        'password' => password_hash($request->all()['password'], PASSWORD_DEFAULT),
-                        'updated_at' => $loginTime,
-                    ];
-                    User::where('monkey_user_id', $response['monkey_user_id'])->update($temp);
+        if (!is_null($findUser)) {
+            if (env('USE_MONKEYID')) { // if using monkey id
+                $response = HttpsRequest::post('https://sports.nsysu.edu.tw/monkeyserver/api/app/login/'.env('MONKEYID_KEY'), $request->all());
+                if ($response['status'] == 'A02') { // check response
+                    if (is_null($findUser)) { // add to user table
+                        $temp = [
+                            'account' => $response['account'],
+                            'monkey_user_id' => $response['monkey_user_id'],
+                            'user_identity' => $response['user_identity'],
+                            'first_name_ch' => $response['name'],
+                            'org_code' => $response['org_code'],
+                            'password' => password_hash($request->all()['password'], PASSWORD_DEFAULT),
+                            'is_student' => $response['user_identity'] == 0 ? 1 : 0,
+                            'created_at' => $loginTime,
+                            'updated_at' => $loginTime,
+                            'athlete_id' => strtoupper(str_pad(base_convert(floor(microtime(true)*100), 10, 36), 8, '0', STR_PAD_LEFT)),
+                        ];
+                        User::insert($temp);
+                    } else { // update user table
+                        $temp = [
+                            'account' => $response['account'],
+                            'user_identity' => $response['user_identity'],
+                            'org_code' => $response['org_code'],
+                            'is_student' => $response['user_identity'] == 0 ? 1 : 0,
+                            'password' => password_hash($request->all()['password'], PASSWORD_DEFAULT),
+                            'updated_at' => $loginTime,
+                        ];
+                        User::where('monkey_user_id', $response['monkey_user_id'])->update($temp);
+                    }
+                } else {
+                    $response['from'] = 'monkey_id';
+                    return response()->json($response, 200);
                 }
-            } else {
-                $response['from'] = 'monkey_id';
-                return response()->json($response, 200);
+            } else { // not using monkey id
+                return response()->json(['status' => 'U02', 'message' => '請先註冊帳號', 'from' => 'sep5'], 200);
             }
-        } else { // not using monkey id
+        } else { // not found
             return response()->json(['status' => 'U02', 'message' => '請先註冊帳號', 'from' => 'sep5'], 200);
         }
         // get user data again
