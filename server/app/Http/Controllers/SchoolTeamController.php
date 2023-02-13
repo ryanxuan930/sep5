e6d48346cdd24aedd1b97b9cccac954270685d55
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-13 17:32:10 +0800

    Update SchoolTeamController.php

--- server/app/Http/Controllers/SchoolTeamController.php
+++ server/app/Http/Controllers/SchoolTeamController.php
@@ -12,7 +12,7 @@ class SchoolTeamController extends Controller
     // construct
     public function __construct()
     {
-        $this->middleware('auth:admin', ['except' => ['index', 'show']]);
+        $this->middleware('auth:admin', ['except' => ['index', 'indexAll', 'show']]);
     }
     /**
      * Display a listing of the resource.
