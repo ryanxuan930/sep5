3537be0c52b258f2b54f1e8def6522e49994160c
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-18 20:14:49 +0800

    Create 2023_02_18_121348_add_num_in_dept_to_users.php

---
+++ server/database/migrations/2023_02_18_121348_add_num_in_dept_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('num_in_dept')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('num_in_dept');
+        });
+    }
+};
