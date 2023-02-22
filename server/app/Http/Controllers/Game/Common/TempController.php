667fa106f7c0b072c825a0ec160cb9f956a7c1e4
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-22 18:51:43 +0800

    Update TempController.php

--- server/app/Http/Controllers/Game/Common/TempController.php
+++ server/app/Http/Controllers/Game/Common/TempController.php
@@ -52,7 +52,7 @@ class TempController extends Controller
      */
     public function show($sportCode, $gameId, $key)
     {
-        return response()->json(DB::table($sportCode.'_'.$gameId.'_'.$this->tableName)->where('key', $key)->first());
+        return response()->json(DB::table($sportCode.'_'.$gameId.'_'.$this->tableName)->where('temp_key', $key)->first());
     }
 
     /**
@@ -71,7 +71,7 @@ class TempController extends Controller
             return response()->json($validator->errors(), 400);
         }
         $temp = $request->all();
-        DB::table($sportCode.'_'.$gameId.'_'.$this->tableName)->where('temp_key', '=' , strval($key))->update($temp);
+        DB::table($sportCode.'_'.$gameId.'_'.$this->tableName)->where('temp_key', '=' , $key)->update($temp);
         return response()->json(['status'=>'A01']);
     }
 
