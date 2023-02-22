e37441b5460bfb886a655627166b9fc8c88a90f2
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-22 15:52:40 +0800

    Create EditConsentFormat.vue

---
+++ sites/src/components/admin/game/registration/EditConsentFormat.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import { ref, reactive, watch } from 'vue';
+  import VueRequest from '@/vue-request';
+  import { useUserStore } from '@/stores/user';
+  import { useRoute } from 'vue-router';
+
+  const store = useUserStore();
+  const vr = new VueRequest(store.token);
+  const route = useRoute();
+
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped lang="scss">
+    
+</style>
