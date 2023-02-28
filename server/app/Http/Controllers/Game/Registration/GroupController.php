b61268973f92acfbb6123768a13887b4d15d24d5
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-28 13:44:25 +0800

    Update GroupController.php

--- server/app/Http/Controllers/Game/Registration/GroupController.php
+++ server/app/Http/Controllers/Game/Registration/GroupController.php
@@ -60,9 +60,9 @@ class GroupController extends Controller
         $temp = DB::table($sportCode.'_'.$gameId.'_'.$this->tableName)->leftJoin($sportCode.'_'.$gameId.'_teams', $sportCode.'_'.$gameId.'_teams.team_id', '=', $sportCode.'_'.$gameId.'_'.$this->tableName.'.team_id')->select($sportCode.'_'.$gameId.'_teams.team_id', $sportCode.'_'.$gameId.'_teams.member_list')->get();
         $userArray = array();
         foreach ($temp as $row) {
-            array_merge($userArray, json_decode($row->member_list, true));
+            $userArray = array_merge($userArray, json_decode($row->member_list, true));
         }
-        return response()->json($temp);
+        return response()->json($userArray);
     }
     public function indexByCount($sportCode, $gameId, $unit)
     {
