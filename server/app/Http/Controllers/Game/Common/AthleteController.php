18bee977c2b20c556a1c342822d07ce6e51324a0
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-02-28 13:58:08 +0800

    Update AthleteController.php

--- server/app/Http/Controllers/Game/Common/AthleteController.php
+++ server/app/Http/Controllers/Game/Common/AthleteController.php
@@ -20,7 +20,7 @@ class AthleteController extends Controller
         foreach ($tempIndividual as $row) {
             array_push($userArray, $row->u_id);
         }
-        $tempGroup = DB::table($sportCode.'_'.$gameId.'_groups')->leftJoin($sportCode.'_'.$gameId.'_teams', $sportCode.'_'.$gameId.'_teams.team_id', '=', $sportCode.'_'.$gameId.'_'.$this->tableName.'.team_id')->select($sportCode.'_'.$gameId.'_teams.team_id', $sportCode.'_'.$gameId.'_teams.member_list')->get();
+        $tempGroup = DB::table($sportCode.'_'.$gameId.'_groups')->leftJoin($sportCode.'_'.$gameId.'_teams', $sportCode.'_'.$gameId.'_teams.team_id', '=', $sportCode.'_'.$gameId.'_groups'.'.team_id')->select($sportCode.'_'.$gameId.'_teams.team_id', $sportCode.'_'.$gameId.'_teams.member_list')->get();
         foreach ($tempGroup as $row) {
             $userArray = array_merge($userArray, json_decode($row->member_list, true));
         }
