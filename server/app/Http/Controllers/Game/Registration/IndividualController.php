608d9ed2bf881cb2c3b2ee5e4552466c0d9fe4c3
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-03-07 17:58:16 +0800

    Update IndividualController.php

--- server/app/Http/Controllers/Game/Registration/IndividualController.php
+++ server/app/Http/Controllers/Game/Registration/IndividualController.php
@@ -63,7 +63,7 @@ class IndividualController extends Controller
         ->leftJoin('departments', 'users.dept_id', '=', 'departments.dept_id')
         ->leftJoin('events', 'events.event_code', '=', $sportCode.'_'.$gameId.'_'.$this->tableName.'.event_code')
         ->select($sportCode.'_'.$gameId.'_'.$this->tableName.'.*', 'users.first_name_ch', 'users.last_name_ch', 'users.first_name_en', 'users.last_name_en', 'users.org_code', 'users.dept_id', 'users.sex', 'users.num_in_dept', 'organizations.org_name_full_ch', 'organizations.org_name_ch', 'organizations.org_name_full_en', 'organizations.org_name_en', 'departments.dept_name_ch', 'departments.dept_name_en', 'events.event_ch', 'events.event_en', 'events.event_jp', 'events.event_abbr', $sportCode.'_'.$gameId.'_divisions.*');
-        return response()->json($query->where($sportCode.'_'.$gameId.'_'.$this->tableName.'.division_id', $divisionId)->where($sportCode.'_'.$gameId.'_'.$this->tableName.'.event_code', $eventCode));
+        return response()->json($query->where($sportCode.'_'.$gameId.'_'.$this->tableName.'.division_id', $divisionId)->where($sportCode.'_'.$gameId.'_'.$this->tableName.'.event_code', $eventCode)->get());
     }
     public function indexByCount($sportCode, $gameId, $unit)
     {
