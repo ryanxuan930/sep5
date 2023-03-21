ee498e29cb23fda1d3b5601661b19ba4607f9433
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-03-21 15:04:52 +0800

    Update ImportAthlete.vue

--- sites/src/components/registration/module/ImportAthlete.vue
+++ sites/src/components/registration/module/ImportAthlete.vue
@@ -53,7 +53,7 @@ function check() {
     if (uploadData.value[i].is_student != 1) {
       uploadData.value[i].is_student = 0;
     }
-    if (Number.isInteger(Number(uploadData.value[i].dept_id)) || uploadData.value[i].dept_id == null || isNaN(Number(uploadData.value[i].dept_id))) {
+    if (Number.isInteger(Number(uploadData.value[i].dept_id)) || uploadData.value[i].dept_id == null) {
       uploadData.value[i].dept_id = store.userInfo.dept_id;
     } else {
       uploadData.value[i].dept_id = Number(uploadData.value[i].dept_id);
