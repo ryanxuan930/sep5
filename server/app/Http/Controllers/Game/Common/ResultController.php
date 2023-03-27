c423745420e61e961aa3eae0ae88e3ba460587e8
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-03-27 18:32:28 +0800

    Update ResultController.php

--- server/app/Http/Controllers/Game/Common/ResultController.php
+++ server/app/Http/Controllers/Game/Common/ResultController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Game\Common;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
+use Illuminate\Support\Facades\DB;
 
 class ResultController extends Controller
 {
