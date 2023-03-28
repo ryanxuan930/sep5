d71cdc0804d236cc8bc16330996b87a13b594929
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-03-28 20:21:57 +0800

    Update ResultController.php

--- server/app/Http/Controllers/Game/Common/ResultController.php
+++ server/app/Http/Controllers/Game/Common/ResultController.php
@@ -18,7 +18,7 @@ class ResultController extends Controller
         $tempGroup = json_decode(json_encode($tempGroup), true, 512, JSON_BIGINT_AS_STRING);
         for ($i = 0; $i < count($tempIndividual); $i++) {
             for ($j = 0; $j < count($tempGroup); $j++) {
-                if ($tempGroup[$i]['org_code'] == $tempGroup[$j]['org_code'] && $tempIndividual[$i]['dept_id'] == $tempGroup[$j]['dept_id'] && $tempIndividual[$i]['r4_ranking'] == $tempGroup[$j]['r4_ranking']) {
+                if ($tempGroup[$i]['org_code'] == $tempGroup[$j]['org_code'] && $tempIndividual[$i]['dept_id'] == $tempGroup[$j]['dept_id'] && $tempIndividual[$i][r4_ranking] == $tempGroup[$j][r4_ranking]) {
                     $tempIndividual[$i]['count'] += $tempGroup[$j]['count'];
                     array_splice($tempGroup, $j, 1);
                     break;
