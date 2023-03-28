68a1017853cb83cd985e2da4fa7be80b2cff4f7f
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-03-28 20:09:31 +0800

    Update ResultController.php

--- server/app/Http/Controllers/Game/Common/ResultController.php
+++ server/app/Http/Controllers/Game/Common/ResultController.php
@@ -13,7 +13,9 @@ class ResultController extends Controller
     {
         $userArray = array();
         $tempIndividual = DB::table($sportCode.'_'.$gameId.'_individuals')->leftJoin('users', 'users.u_id', '=', $sportCode.'_'.$gameId.'_individuals.u_id')->leftJoin('organizations', 'organizations.org_code', '=', 'users.org_code')->leftJoin('departments', 'departments.dept_id', '=', 'users.dept_id')->select('users.org_code', 'users.dept_id', 'organizations.org_name_full_ch', 'organizations.org_name_ch', 'organizations.org_name_full_en', 'organizations.org_name_en', 'departments.dept_name_ch', 'departments.dept_name_en', $sportCode.'_'.$gameId.'_individuals.r4_ranking', DB::raw('count(*) as count'))->where($sportCode.'_'.$gameId.'_individuals.r4_ranking', '<=', 8)->where($sportCode.'_'.$gameId.'_individuals.r4_ranking', '>', 0)->groupBy('users.org_code', 'users.dept_id', $sportCode.'_'.$gameId.'_individuals.r4_ranking')->get();
+        $tempIndividual = json_decode(json_encode($tempIndividual), true);
         $tempGroup = DB::table($sportCode.'_'.$gameId.'_groups')->leftJoin($sportCode.'_'.$gameId.'_teams', $sportCode.'_'.$gameId.'_groups.team_id', '=', $sportCode.'_'.$gameId.'_teams.team_id')->leftJoin('organizations', 'organizations.org_id', '=', $sportCode.'_'.$gameId.'_teams.org_id')->leftJoin('departments', 'departments.dept_id', '=', $sportCode.'_'.$gameId.'_teams.dept_id')->select($sportCode.'_'.$gameId.'_teams.org_id', $sportCode.'_'.$gameId.'_teams.dept_id', 'organizations.org_code', 'organizations.org_name_full_ch', 'organizations.org_name_ch', 'organizations.org_name_full_en', 'organizations.org_name_en', 'departments.dept_name_ch', 'departments.dept_name_en', $sportCode.'_'.$gameId.'_groups.r4_ranking', DB::raw('count(*) as count'))->where($sportCode.'_'.$gameId.'_groups.r4_ranking', '<=', 8)->where($sportCode.'_'.$gameId.'_groups.r4_ranking', '>', 0)->groupBy($sportCode.'_'.$gameId.'_teams.org_id', $sportCode.'_'.$gameId.'_teams.dept_id', $sportCode.'_'.$gameId.'_groups.r4_ranking')->get();
+        $tempGroup = json_decode(json_encode($tempGroup), true);
         for ($i = 0; $i < count($tempIndividual); $i++) {
             for ($j = 0; $j < count($tempGroup); $j++) {
                 if ($tempIndividual[$i]->org_code == $tempGroup[$j]->org_code && $tempIndividual[$i]->dept_id == $tempGroup[$j]->dept_id && $tempIndividual[$i]->r4_ranking == $tempGroup[$j]->r4_ranking) {
@@ -23,7 +25,7 @@ class ResultController extends Controller
                 }
             }
         }
-        $userArray = array_merge(json_decode(json_encode($tempIndividual), true), json_decode(json_encode($tempGroup), true));
+        $userArray = array_merge($tempIndividual, $tempGroup);
         $col1 = array_column($userArray, 'org_code');
         $col2 = array_column($userArray, 'dept_id');
         $col3 = array_column($userArray, 'r4_ranking');
