cdcdcaeb36db4fb159ead8bb33584661bcbe7646
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-03-31 13:08:02 +0800

    Update ResultController.php

--- server/app/Http/Controllers/Game/Common/ResultController.php
+++ server/app/Http/Controllers/Game/Common/ResultController.php
@@ -89,7 +89,7 @@ class ResultController extends Controller
         $index = -1;
         $userArray = array();
         for ($i = 0; $i < count($dataArray); $i++) {
-            if ($dataArray[$i]['org_code'] != $tempOrg || $dataArray[$i]['dept_id'] != $tempDept || $dataArray[$i]['r4_ranking'] != $tempRank) {
+            if (($dataArray[$i]['org_code'] != $tempOrg || $dataArray[$i]['dept_id'] != $tempDept) && $dataArray[$i]['r4_ranking'] != $tempRank) {
                 $index++;
                 $tempOrg = $dataArray[$i]['org_code'];
                 $tempDept = $dataArray[$i]['dept_id'];
