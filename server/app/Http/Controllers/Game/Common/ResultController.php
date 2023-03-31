311277fe2926d1fe024da6849db0f9430b43b458
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-03-31 13:23:56 +0800

    Update ResultController.php

--- server/app/Http/Controllers/Game/Common/ResultController.php
+++ server/app/Http/Controllers/Game/Common/ResultController.php
@@ -17,69 +17,7 @@ class ResultController extends Controller
         $tempIndividual = json_decode(json_encode($tempIndividual), true, 512, JSON_BIGINT_AS_STRING);
         $tempGroup = DB::table($sportCode.'_'.$gameId.'_groups')->leftJoin($sportCode.'_'.$gameId.'_teams', $sportCode.'_'.$gameId.'_groups.team_id', '=', $sportCode.'_'.$gameId.'_teams.team_id')->leftJoin($sportCode.'_'.$gameId.'_divisions', $sportCode.'_'.$gameId.'_groups.division_id', '=', $sportCode.'_'.$gameId.'_divisions.division_id')->leftJoin('events', $sportCode.'_'.$gameId.'_groups.event_code', '=', 'events.event_code')->leftJoin('organizations', 'organizations.org_id', '=', $sportCode.'_'.$gameId.'_teams.org_id')->leftJoin('departments', 'departments.dept_id', '=', $sportCode.'_'.$gameId.'_teams.dept_id')->select($sportCode.'_'.$gameId.'_teams.org_id', $sportCode.'_'.$gameId.'_teams.dept_id', 'organizations.org_code', 'organizations.org_name_full_ch', 'organizations.org_name_ch', 'organizations.org_name_full_en', 'organizations.org_name_en', 'departments.dept_name_ch', 'departments.dept_name_en', $sportCode.'_'.$gameId.'_divisions.division_ch', $sportCode.'_'.$gameId.'_divisions.division_en', $sportCode.'_'.$gameId.'_divisions.division_id', 'events.event_ch', 'events.event_en', 'events.event_code', 'events.event_id', $sportCode.'_'.$gameId.'_groups.r4_ranking', DB::raw('count(*) as count'))->where($sportCode.'_'.$gameId.'_groups.r4_ranking', '<=', $num)->where($sportCode.'_'.$gameId.'_groups.r4_ranking', '>', 0)->groupBy($sportCode.'_'.$gameId.'_teams.org_id', $sportCode.'_'.$gameId.'_teams.dept_id', $sportCode.'_'.$gameId.'_groups.r4_ranking')->get();
         $tempGroup = json_decode(json_encode($tempGroup), true, 512, JSON_BIGINT_AS_STRING);
-        /*
-        $groups = [];
-        for ($i = 0; $i < count($tempIndividual); $i++) {
-            for ($j = 0; $j < count($tempGroup); $j++) {
-                if ($tempIndividual[$i]['org_code'] == $tempGroup[$j]['org_code'] && $tempIndividual[$i]['dept_id'] == $tempGroup[$j]['dept_id']) {
-                    if ($tempIndividual[$i]['r4_ranking'] == $tempGroup[$j]['r4_ranking']) {
-                        $tempIndividual[$i]['count'] += $tempGroup[$j]['count'];
-                    } else {
-                        $tempIndividual[$i]['count'] = $tempGroup[$j]['count'];
-                    }
-                    continue;
-                } else {
-                    array_push($groups, $tempGroup[$j]);
-                }
-            }
-        }
-        $userArray = array_merge($tempIndividual, $groups);
-        $col1 = array_column($userArray, 'org_code');
-        $col2 = array_column($userArray, 'dept_id');
-        $col3 = array_column($userArray, 'r4_ranking');
-        array_multisort($userArray, SORT_ASC, $col1, SORT_ASC, $col2, SORT_ASC, $col3, SORT_ASC);*/
         $dataArray = array();
-        /*
-        for ($i = 0; $i < count($tempIndividual); $i++) {
-            array_push($dataArray, [
-                'org_code' => $tempIndividual[$i]['org_code'],
-                'dept_id' => $tempIndividual[$i]['dept_id'],
-                'org_name_full_ch' => $tempIndividual[$i]['org_name_full_ch'],
-                'org_name_ch' => $tempIndividual[$i]['org_name_ch'],
-                'org_name_full_en' => $tempIndividual[$i]['org_name_full_en'],
-                'org_name_en' => $tempIndividual[$i]['org_name_en'],
-                'dept_name_ch' => $tempIndividual[$i]['dept_name_ch'],
-                'dept_name_en' => $tempIndividual[$i]['dept_name_en'],
-                'division_ch' => $tempIndividual[$i]['division_ch'],
-                'division_en' => $tempIndividual[$i]['division_en'],
-                'division_id' => $tempIndividual[$i]['division_id'],
-                'event_ch' => $tempIndividual[$i]['event_ch'],
-                'event_en' => $tempIndividual[$i]['event_en'],
-                'event_code' => $tempIndividual[$i]['event_code'],
-                'r4_ranking' => $tempIndividual[$i]['r4_ranking'],
-                'count' => $tempIndividual[$i]['count'],
-            ]);
-        }
-        for( $i = 0; $i < count($tempGroup); $i++ ) {
-            array_push($dataArray, [
-                'org_code' => $tempGroup[$i]['org_code'],
-                'dept_id' => $tempGroup[$i]['dept_id'],
-                'org_name_full_ch' => $tempGroup[$i]['org_name_full_ch'],
-                'org_name_ch' => $tempGroup[$i]['org_name_ch'],
-                'org_name_full_en' => $tempGroup[$i]['org_name_full_en'],
-                'org_name_en' => $tempGroup[$i]['org_name_en'],
-                'dept_name_ch' => $tempGroup[$i]['dept_name_ch'],
-                'dept_name_en' => $tempGroup[$i]['dept_name_en'],
-                'division_ch' => $tempGroup[$i]['division_ch'],
-                'division_en' => $tempGroup[$i]['division_en'],
-                'division_id' => $tempGroup[$i]['division_id'],
-                'event_ch' => $tempGroup[$i]['event_ch'],
-                'event_en' => $tempGroup[$i]['event_en'],
-                'event_code' => $tempGroup[$i]['event_code'],
-                'r4_ranking' => $tempGroup[$i]['r4_ranking'],
-                'count' => $tempGroup[$i]['count'],
-            ]);
-        }*/
         $dataArray = array_merge($tempIndividual, $tempGroup);
         $col1 = array_column($dataArray, 'org_code');
         $col2 = array_column($dataArray, 'dept_id');
@@ -87,18 +25,53 @@ class ResultController extends Controller
         array_multisort($dataArray, SORT_ASC, $col1, SORT_ASC, $col2, SORT_ASC, $col3, SORT_ASC);
         $tempOrg = '';
         $tempDept = '';
-        $tempRank = 0;
-        $index = -1;
-        $userArray = array();
+        $tempArray = array();
         for ($i = 0; $i < count($dataArray); $i++) {
-            if (($dataArray[$i]['org_code'] != $tempOrg || $dataArray[$i]['dept_id'] != $tempDept) && $dataArray[$i]['r4_ranking'] != $tempRank) {
-                $index++;
+            if ($dataArray[$i]['org_code'] != $tempOrg || $dataArray[$i]['dept_id'] != $tempDept) {
                 $tempOrg = $dataArray[$i]['org_code'];
                 $tempDept = $dataArray[$i]['dept_id'];
-                $tempRank = $dataArray[$i]['r4_ranking'];
-                array_push($userArray, $dataArray[$i]);
+                array_push($tempArray, $dataArray[$i]);
             }
-            $userArray[$index]['count'] += $dataArray[$i]['count'];
+        }
+        for ($i = 0; $i < count($userArray); $i++) {
+            for ($j = 0; $j < $num; $j++) {
+                $count = 0;
+                $flag = false;
+                for ($k = 0; $k < count($tempIndividual); $k++) {
+                    if ($tempIndividual[$k]['org_code'] == $tempArray[$i]['org_code'] && $tempIndividual[$k]['dept_id'] == $tempArray[$i]['dept_id'] && $tempIndividual[$j]['r4_ranking'] == $j + 1) {
+                        $count += $tempIndividual[$j]['count'];
+                        $flag = true;
+                    }
+                }
+                for ($k = 0; $k < count($tempGroup); $k++) {
+                    if ($tempGroup[$k]['org_code'] == $tempArray[$i]['org_code'] && $tempGroup[$k]['dept_id'] == $tempArray[$i]['dept_id'] && $tempGroup[$j]['r4_ranking'] == $j + 1) {
+                        $count += $tempGroup[$j]['count'];
+                        $flag = true;
+                    }
+                }
+                if ($flag) {
+                    array_push($userArray, [
+                        'org_code' => $tempArray[$i]['org_code'],
+                        'dept_id' => $tempArray[$i]['dept_id'],
+                        'org_name_full_ch' => $tempArray[$i]['org_name_full_ch'],
+                        'org_name_ch' => $tempArray[$i]['org_name_ch'],
+                        'org_name_full_en' => $tempArray[$i]['org_name_full_en'],
+                        'org_name_en' => $tempArray[$i]['org_name_en'],
+                        'dept_name_ch' => $tempArray[$i]['dept_name_ch'],
+                        'dept_name_en' => $tempArray[$i]['dept_name_en'],
+                        'division_ch' => $tempArray[$i]['division_ch'],
+                        'division_en' => $tempArray[$i]['division_en'],
+                        'division_id' => $tempArray[$i]['division_id'],
+                        'event_ch' => $tempArray[$i]['event_ch'],
+                        'event_en' => $tempArray[$i]['event_en'],
+                        'event_code' => $tempArray[$i]['event_code'],
+                        'event_id' => $tempArray[$i]['event_id'],
+                        'r4_ranking' => $j + 1,
+                        'count' => $count,
+                    ]);
+                }
+            }
+
         }
         return response()->json($userArray);
     }
